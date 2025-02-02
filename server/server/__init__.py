02df8992e7131f76ea6ce3042c0c2d15c395c732
tech-master823 <carlsh0823@gmail.com>
2025-02-02 12:32:00 -0800

    firebase-connect

--- server/server/__init__.py
+++
@@ -1,5 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("C:/Users/master-0823/Documents/work/task/server/server/inhousems-aee24-firebase-adminsdk-fbsvc-68aee5da0f.json")
-firebase_admin.initialize_app(cred)
