22a7aaf7dc618f703847d138c148245df6bf6880
tech-master823 <carlsh0823@gmail.com>
2025-02-08 18:28:57 -0800

    to-env

--- server/server/settings.py
+++ server/server/settings.py
@@ -176,6 +176,23 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_KEY_FILE)
+from decouple import config
+
+# Google Cloud Service Account credentials
+GOOGLE_CREDENTIALS = {
+    "type": config('TYPE'),
+    "project_id": config('PROJECT_ID'),
+    "private_key_id": config('PRIVATE_KEY_ID'),
+    "private_key": config('PRIVATE_KEY').replace('\\n', '\n'),  # Make sure to format the key correctly
+    "client_email": config('CLIENT_EMAIL'),
+    "client_id": config('CLIENT_ID'),
+    "auth_uri": config('AUTH_URI'),
+    "token_uri": config('TOKEN_URI'),
+    "auth_provider_x509_cert_url": config('AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": config('CLIENT_X509_CERT_URL'),
+    "universe_domain": config('UNIVERSE_DOMAIN'),
+}
+
+cred = credentials.Certificate(GOOGLE_CREDENTIALS)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
