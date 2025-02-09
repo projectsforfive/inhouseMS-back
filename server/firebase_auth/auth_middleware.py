e3a90e139f129d7e8ec38ec3f8ab3f5f1432a13b
tech-master823 <carlsh0823@gmail.com>
2025-02-08 18:54:56 -0800

    readme-change

--- server/firebase_auth/auth_middleware.py
+++ server/firebase_auth/auth_middleware.py
@@ -4,18 +4,18 @@ from rest_framework import status
 from firebase_admin import auth
 
 def firebase_auth_required(view_func, *arg, **karg):
-    @wraps(view_func)
-    def _wraped_view(request, *arg, **karg):
-        id_token = request.headers.get('Authorization')
-        
-        if not id_token:
-            return Response({ 'success': False, 'message': 'Unauthorized' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        try:
-            decoded_token = auth.verify_id_token(id_token)
-            request.user = decoded_token
-            view_func(request, *arg, **karg)
-        except Exception as e:
-            pass
-    
-    return _wraped_view
+	@wraps(view_func)
+	def _wraped_view(request, *arg, **karg):
+		id_token = request.headers.get('Authorization')
+		
+		if not id_token:
+			return Response({ 'success': False, 'message': 'Unauthorized' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+		
+		try:
+			decoded_token = auth.verify_id_token(id_token)
+			request.user = decoded_token
+			view_func(request, *arg, **karg)
+		except Exception as e:
+			pass
+	
+	return _wraped_view
--- server/firebase_auth/views.py
+++ server/firebase_auth/views.py
@@ -15,12 +15,11 @@ class Register(APIView):
   def post(self, request):
     try:    
       db = firestore.client()
-      # uid = request.data['uid']
       user_ref = db.collection('user')
       user_ref.add({
-          'username': request.data["username"], 
-          'email': request.data["email"],
-          'password': request.data["password"],
+        'username': request.data["username"], 
+        'email': request.data["email"],
+        'password': request.data["password"],
       })
     except KeyError:
         return Response({'message': "Error: The key does not exist in the request."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
